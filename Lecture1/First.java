e39926e69dabe58a13576ae93025ee4460f77a67
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2024-08-29 11:56:12 +0530

    Lecture1

---
+++ Lecture1/First.java
@@ -0,0 +1,11 @@
+package Lecture1;
+
+public class First 
+{
+public static void main(String args[])
+{
+		System.out.println("Hello worldddd");
+		System.out.println("bye");
+System.out.println("inside main");
+}
+}
---
+++ Lecture1/LearningAdd.java
@@ -0,0 +1,17 @@
+package Lecture1;
+
+public class LearningAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int c=30;
+int sum=a+b+c;
+System.out.println(sum);
+int val=a>b?a:b;
+System.out.println(val);
+
+	}
+
+}
