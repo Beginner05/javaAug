04daf43de649b947d1727593c246ae76815e073f
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2024-08-29 12:01:37 +0530

    Lecture6

---
+++ Lecture6/Pattern2.java
@@ -0,0 +1,16 @@
+package Lecture6;
+
+public class Pattern2 {
+
+	public static void main(String args[])
+	{
+		for(int rows=0; rows<5; rows++) {
+			for(int cst=0; cst<5; cst++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+	}
+}
